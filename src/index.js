ad894a389b27ff276a433fefd792724dc1991d5b
Keizt <keyzzzt@gmail.com>
2021-03-09 20:43:22 +0200

    major changes done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,144 @@
 import './styles/style.scss'
 
+window.onload = function () {
+
+    let info = document.querySelector('.footer__information');
+    let infoTitle = document.querySelector('.info__title');
+    let infoList = document.querySelector('.footer__information ul');
+    let infoArrow = document.querySelector('.footer__information div');
+
+    let individuals = document.querySelector('.footer__individuals');
+    let individualsTitle = document.querySelector('.ind__title');
+    let individualsList = document.querySelector('.footer__individuals ul');
+    let individualsArrow = document.querySelector('.footer__individuals div');
+
+    let business = document.querySelector('.footer__business');
+    let businessTitle = document.querySelector('.bus__title');
+    let businessList = document.querySelector('.footer__business ul');
+    let busArrow = document.querySelector('.footer__business div');
+
+    let company = document.querySelector('.footer__company');
+    let companyTitle = document.querySelector('.com__title');
+    let companyList = document.querySelector('.footer__company ul');
+    let comArrow = document.querySelector('.footer__company div');
+
+    let burgerFees = document.querySelector('.burger__fees-title');
+    let feesList = document.querySelector('.burger__fees-list');
+
+    let burgerYoxPay = document.querySelector('.burger__yoxpay-title');
+    let YoxPayList = document.querySelector('.burger__yoxpay-list');
+
+    let langBurger = document.querySelector('.burger-lang')
+    let langBurgerList = document.querySelector('.lang__list-burger')
+
+    let langBottom = document.querySelector('.lang-wrapper div')
+    let langBottomList = document.querySelector('.lang__list')
+    let langArrow = document.querySelector('.triangle-down-dark')
+
+    let openBurger = document.querySelector('.header__burger')
+    let closeBurger = document.querySelector('.header__close')
+    let burgerMenu = document.querySelector('.ham')
+
+    closeBurger.addEventListener('click', () => {
+        burgerMenu.classList.remove('display-block')
+    })
+
+    openBurger.addEventListener('click', () => {
+        burgerMenu.classList.add('display-block')
+    })
+
+    langBurger.addEventListener('click', () => {
+        langBurgerList.classList.toggle('display-block')
+        // if (langArrow.classList == 'triangle-down-dark') {
+        //     langArrow.classList.add('triangle-up-dark')
+        //     langArrow.classList.remove('triangle-down-dark')
+        // } else {
+        //     langArrow.classList.remove('triangle-up-dark')
+        //     langArrow.classList.add('triangle-down-dark')
+        // }
+    })
+
+    burgerYoxPay.addEventListener('click', () => {
+        YoxPayList.classList.toggle('display-block')
+        // if (langArrow.classList == 'triangle-down-dark') {
+        //     langArrow.classList.add('triangle-up-dark')
+        //     langArrow.classList.remove('triangle-down-dark')
+        // } else {
+        //     langArrow.classList.remove('triangle-up-dark')
+        //     langArrow.classList.add('triangle-down-dark')
+        // }
+    })
+
+    burgerFees.addEventListener('click', () => {
+        feesList.classList.toggle('display-block')
+        // if (langArrow.classList == 'triangle-down-dark') {
+        //     langArrow.classList.add('triangle-up-dark')
+        //     langArrow.classList.remove('triangle-down-dark')
+        // } else {
+        //     langArrow.classList.remove('triangle-up-dark')
+        //     langArrow.classList.add('triangle-down-dark')
+        // }
+    })
+
+    langBottom.addEventListener('click', () => {
+        langBottomList.classList.toggle('display-block')
+        if (langArrow.classList == 'triangle-down-dark') {
+            langArrow.classList.add('triangle-up-dark')
+            langArrow.classList.remove('triangle-down-dark')
+        } else {
+            langArrow.classList.remove('triangle-up-dark')
+            langArrow.classList.add('triangle-down-dark')
+        }
+    })
+
+
+    individuals.addEventListener('click', () => {
+        individualsList.classList.toggle('display-block');
+        individualsTitle.classList.toggle('active-footermenu-color');
+        if (individualsArrow.classList == 'triangle-down-gray') {
+            individualsArrow.classList.add('triangle-up-dark')
+            individualsArrow.classList.remove('triangle-down-gray')
+        } else {
+            individualsArrow.classList.remove('triangle-up-dark')
+            individualsArrow.classList.add('triangle-down-gray')
+        }
+    });
+    info.addEventListener('click', (e) => {
+        infoList.classList.toggle('display-block');
+        infoTitle.classList.toggle('active-footermenu-color');
+        if (infoArrow.classList == 'triangle-down-gray') {
+            infoArrow.classList.add('triangle-up-dark')
+            infoArrow.classList.remove('triangle-down-gray')
+        } else {
+            infoArrow.classList.remove('triangle-up-dark')
+            infoArrow.classList.add('triangle-down-gray')
+        }
+
+
+    });
+    business.addEventListener('click', (e) => {
+        businessList.classList.toggle('display-block');
+        businessTitle.classList.toggle('active-footermenu-color');
+        if (busArrow.classList == 'triangle-down-gray') {
+            busArrow.classList.add('triangle-up-dark')
+            busArrow.classList.remove('triangle-down-gray')
+        } else {
+            busArrow.classList.remove('triangle-up-dark')
+            busArrow.classList.add('triangle-down-gray')
+        }
+    });
+    company.addEventListener('click', (e) => {
+        companyList.classList.toggle('display-block');
+        companyTitle.classList.toggle('active-footermenu-color');
+        if (comArrow.classList == 'triangle-down-gray') {
+            comArrow.classList.add('triangle-up-dark')
+            comArrow.classList.remove('triangle-down-gray')
+        } else {
+            comArrow.classList.remove('triangle-up-dark')
+            comArrow.classList.add('triangle-down-gray')
+        }
+    });
+
+
+
+}
